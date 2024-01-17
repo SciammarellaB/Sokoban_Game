4a641ff440820bbf6aa30cb61671259a41ae7b8c
Brenno <brenno@giomtecnologia.com.br>
2024-01-17 20:21:09 -0300

    Added GLEW and Docs

--- main.cpp
+++ main.cpp
@@ -67,6 +67,8 @@ void loadTexture() {
 
     unsigned int texture;
     glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
@@ -74,7 +76,6 @@ void loadTexture() {
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data);
     glGenerateMipmap(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture);
 
     if(img_data)
         stbi_image_free(img_data);
@@ -378,7 +379,7 @@ int main()
         game.lastFrame = game.currentFrame;
 
         //RUN GAME
-        update();
+        //update();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
